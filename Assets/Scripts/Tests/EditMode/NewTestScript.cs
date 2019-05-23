06d1fc0edfd4129c84d68fd73a4fb097a6a444c7
Riley Moss <riley.moss@accenture.com>
2019-05-23 10:39:00 -0500

    Added Edit Mode and Play Mode tests
    Signed-off-by: Rob Murdock <robert.f.murdock@accenture.com>

---
+++ Assets/Scripts/Tests/EditMode/NewTestScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class NewTestScript
+    {
+        [Test]
+        public void NewTestScriptSimplePasses()
+        {
+            Debug.Log("Debuggamadoo");
+        }
+
+        [UnityTest]
+        public IEnumerator NewTestScriptWithEnumeratorPasses()
+        {
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/PlayMode/NewTestScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class NewTestScript
+    {
+        [Test]
+        public void NewTestScriptSimplePasses()
+        {
+            Debug.Log("Sucka MCs");
+        }
+
+        [UnityTest]
+        public IEnumerator NewTestScriptWithEnumeratorPasses()
+        {
+            yield return null;
+        }
+    }
+}
