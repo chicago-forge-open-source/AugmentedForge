624af97b3c8440b530a68efbac979a38c5abef31
Riley Moss <riley.moss@accenture.com>
2019-05-24 15:09:45 -0500

    Initialize commit of SLAM branch
    Signed-off-by: Rob Murdock <robert.f.murdock@accenture.com>

---
+++ Assets/Scripts/Main/OverlayMapInitialize.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Main
+{
+
+    public class OverlayMapInitialize : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        public void Start()
+        {
+            Debug.Log("Start for Overlay Map");
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/EditMode/OverlayMapInitializeTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using Main;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests.EditMode
+{
+    public class OverlayMapInitializeTest
+    {
+        [Test]
+        public void WhenNoCompassDetectedMapIsRotatedToZero()
+        {
+            GameObject map = GameObject.Find("Overlay Map");
+            var overlayMapInitialize = map.GetComponent<OverlayMapInitialize>();
+            overlayMapInitialize.Start();
+        }
+
+        [UnityTest]
+        public IEnumerator NewTestScriptWithEnumeratorPasses()
+        {
+            yield return null;
+        }
+    }
+}
