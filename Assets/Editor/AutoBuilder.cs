d8e59c2c622eac62b1f4accf646c4952fa943a66
Natalie Frecka <natalie.frecka@accenture.com>
2019-06-06 10:24:19 -0500

    adding execute android build command back into build
    Signed-off-by: Riley Moss <riley.moss@accenture.com>

--- Assets/Editor/AutoBuilder.cs
+++ Assets/Editor/AutoBuilder.cs
@@ -8,9 +8,9 @@ namespace Editor
         [MenuItem("File/AutoBuilder/Android")]
         static void PerformBuild()
         {
-            string[] scenes = {"Assets/GoogleARCore/AugmentedForge/Scenes/MapScene.unity"};
+            string[] scenes = {"./Assets/GoogleARCore/AugmentedForge/Scenes/MapScene.unity"};
 
-            string buildPath = "../../../Build/Android";
+            string buildPath = "./Build/Android";
 
             // Create build folder if not yet exists
             Directory.CreateDirectory(buildPath);
