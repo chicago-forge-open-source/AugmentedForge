f496ce1035e48a38643313fdeef2d6e1689cd678
Riley Moss <riley.moss@accenture.com>
2019-06-20 14:37:32 -0500

    Added button to Chicago Map View to return to AR View
    Signed-off-by: Steven Combs <steven.combs@accenture.com>

---
+++ Assets/Tests/PlayMode/MapViewPlayTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+public class MapViewPlayTests
+{
+    [UnityTest]
+    public IEnumerator GivenChicagoARViewButtonClickLoadChicagoARView()
+    {
+        SceneManager.LoadScene("ChicagoMapView");
+        yield return null;
+
+        var overlayMap = GameObject.Find("Overlay Map");
+        var mapViewScript = overlayMap.GetComponent<MapView>();
+
+        mapViewScript.OnClick_LoadARView();
+        
+        yield return new WaitForSeconds(0.1f);
+        
+        Assert.AreEqual("ChicagoARView", SceneManager.GetActiveScene().name);
+    }
+}
