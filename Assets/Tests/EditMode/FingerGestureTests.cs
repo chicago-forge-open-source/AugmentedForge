dbd3d64e5113c0ec2b598c2501c874f3577ac0b4
Jeb Sumeracki <jeb.sumeracki@accenture.com>
2019-07-16 14:37:46 -0500

    added pinch to zoom functionality for map view
    Signed-off-by: Natalie Frecka <natalie.frecka@accenture.com>

--- Assets/Tests/EditMode/FingerGestureTests.cs
+++ Assets/Tests/EditMode/FingerGestureTests.cs
@@ -1,10 +1,12 @@
-﻿using NUnit.Framework;
+﻿using AugmentedForge;
+using Boo.Lang;
+using NUnit.Framework;
 using UnityEngine;
 
 public class FingerGestureTests
 {
     private GameObject _game;
-    
+
     private Camera _camera;
     private FingerGestures _gesturesScript;
 
@@ -14,15 +16,47 @@ public class FingerGestureTests
         _game = new GameObject();
         _camera = _game.AddComponent<Camera>();
         _gesturesScript = _game.AddComponent<FingerGestures>();
+        _gesturesScript.Start();
     }
 
     [Test]
-    public void Update_NoTouchWillNotChangeTheZoom()
+    public void Update_OneTouchWillNotChangeTheZoom()
     {
         var cameraZoom = _camera.fieldOfView;
+        var touch = new Touch {position = new Vector2(0, 1), deltaPosition = new Vector2(2, 4)};
+        _gesturesScript.input = new MockInput(new List<Touch> {touch});
 
         _gesturesScript.Update();
 
         Assert.AreEqual(cameraZoom, _camera.fieldOfView);
+    }
+
+    [Test]
+    public void Update_TwoTouchesWillChangeTheZoom()
+    {
+        var touch1 = new Touch {position = new Vector2(4, 4), deltaPosition = new Vector2(2, 2)};
+        var touch2 = new Touch {position = new Vector2(6, 6), deltaPosition = new Vector2(4, 4)};
+        _gesturesScript.input = new MockInput(new List<Touch> {touch1, touch2});
+
+        _gesturesScript.Update();
+
+        Assert.AreEqual(58.5857849f, _camera.fieldOfView);
+    }
+}
+
+internal class MockInput : IInput
+{
+    private readonly List<Touch> _touches;
+
+    public MockInput(List<Touch> touches)
+    {
+        _touches = touches;
+    }
+
+    public int TouchCount => _touches.Count;
+
+    public Touch GetTouch(int index)
+    {
+        return _touches[index];
     }
 }
