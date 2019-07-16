d39b8506f66ed6544e5079aea4daa97c46e2cb17
Jeb Sumeracki <jeb.sumeracki@accenture.com>
2019-07-16 11:57:30 -0500

    setup for zoom on mapView
    Signed-off-by: Natalie Frecka <natalie.frecka@accenture.com>

---
+++ Assets/Scripts/FingerGestures.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+
+public class FingerGestures : MonoBehaviour
+{
+    public void Update()
+    {
+        
+    }
+}
---
+++ Assets/Tests/EditMode/FingerGestureTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+public class FingerGestureTests
+{
+    private GameObject _game;
+    
+    private Camera _camera;
+    private FingerGestures _gesturesScript;
+
+    [SetUp]
+    public void Setup()
+    {
+        _game = new GameObject();
+        _camera = _game.AddComponent<Camera>();
+        _gesturesScript = _game.AddComponent<FingerGestures>();
+    }
+
+    [Test]
+    public void Update_NoTouchWillNotChangeTheZoom()
+    {
+        var cameraZoom = _camera.fieldOfView;
+
+        _gesturesScript.Update();
+
+        Assert.AreEqual(cameraZoom, _camera.fieldOfView);
+    }
+}
