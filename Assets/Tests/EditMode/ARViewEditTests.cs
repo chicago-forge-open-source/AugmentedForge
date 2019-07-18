9eeb6110586ae15e092eb9fc81ec85d7d5ee57a8
Natalie Frecka <natalie.frecka@accenture.com>
2019-07-18 11:51:51 -0500

    removing failing test to get back to passing build
    Signed-off-by: Jeb Sumeracki <jeb.sumeracki@accenture.com>

--- Assets/Tests/EditMode/ARViewEditTests.cs
+++ Assets/Tests/EditMode/ARViewEditTests.cs
@@ -381,12 +381,6 @@ public class ARViewEditTests
             Is.EqualTo(expectedCameraRotation).Using(_quaternionComparer)
         );
     }
-
-    [Test]
-    public void WillFail()
-    {
-        Assert.IsTrue(false);
-    }
 }
 
 internal class MockCompass : ICompass
--- testParse.sh
+++ testParse.sh
@@ -1,10 +1,8 @@
 result=$(echo 'cat //test-run/@result' | xmllint --shell ./test-results/editMode/results.xml | awk -F'[="]' '!/>/{print $(NF-1)}')
-echo $result
+
 if test "$result" = "Passed"
 then
-	echo "THE PASSING"
 	exit 0;
 else
-	echo "THE FAILING"
 	exit 1;
 fi
