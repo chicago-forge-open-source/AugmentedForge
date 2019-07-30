da09cff16fbf016b017688512aaa53bd65ac1e62
Jeb Sumeracki <jeb.sumeracki@accenture.com>
2019-07-30 14:00:23 -0500

    fixed data loader merge err
    Signed-off-by: Natalie Frecka <natalie.frecka@accenture.com>

--- Assets/Scripts/DataLoader.cs
+++ Assets/Scripts/DataLoader.cs
@@ -10,15 +10,16 @@ namespace Assets.Scripts
             Repositories.MarkerRepository.Save(markers);
         }
 
-    private static Marker[] LoadMarkers()
-    {
-        var makerSpace = new Marker("Maker Space", 30.371f, -26.29f);
-        var crucible = new Marker("Crucible", 27.5f, -11.44f);
-        var focusRoom = new Marker("Focus Room", 27.5f, -8.03f);
-        var kitchen = new Marker("Kitchen", 43.175f, -3.5f);
-        var greaterMHub = new Marker("Greater MHub", 28.875f, 9.262001f);
-        var entryWay = new Marker("Entrance", 30.37f, -3.5f);
+        private static Marker[] LoadMarkers()
+        {
+            var makerSpace = new Marker("Maker Space", 30.371f, -26.29f);
+            var crucible = new Marker("Crucible", 27.5f, -11.44f);
+            var focusRoom = new Marker("Focus Room", 27.5f, -8.03f);
+            var kitchen = new Marker("Kitchen", 43.175f, -3.5f);
+            var greaterMHub = new Marker("Greater MHub", 28.875f, 9.262001f);
+            var entryWay = new Marker("Entrance", 30.37f, -3.5f);
 
-        return new[] {makerSpace, crucible, focusRoom, kitchen, greaterMHub, entryWay};
+            return new[] {makerSpace, crucible, focusRoom, kitchen, greaterMHub, entryWay};
+        }
     }
 }
