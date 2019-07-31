aed9c0ea8c36839a4b4e620ffcc7fd2e53bbbe02
NatalieFrecka <nfrecka@gmail.com>
2019-07-31 14:18:46 -0500

    SERIOUSLY

--- Assets/Scripts/YellowBrickRoad.cs
+++ Assets/Scripts/YellowBrickRoad.cs
@@ -12,44 +12,27 @@ namespace Assets.Scripts
 
         private LineRenderer NewRoad()
         {
-            try
-            {
-                var road = gameObject.AddComponent<LineRenderer>();
-                road.startWidth = 0.5f;
-                road.endWidth = 0.5f;
-                road.startColor = Color.yellow;
-                road.endColor = Color.yellow;
-                road.material = material;
-                Console.WriteLine("FRECKA CREATED ROAD");
-                print("FRECKA CREATED ROAD");
-                Debug.Log("FRECKA CREATED ROAD");
+            var road = gameObject.AddComponent<LineRenderer>();
+            road.startWidth = 0.5f;
+            road.endWidth = 0.5f;
+            road.startColor = Color.yellow;
+            road.endColor = Color.yellow;
+            road.material = material;
 
-                return road;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("FRECKA");
-                Console.WriteLine(e);
-                print("FRECKA");
-                print(e.ToString());
-                Debug.Log("FRECKA");
-                Debug.Log(e.ToString());
-            }
-
-            return null;
+            return road;
         }
 
         public void PathToDraw(IEnumerable<RoadPoint> vertices)
         {
-            var safeVertices = vertices.Where(vertex => vertex != null).ToArray();
-            if (safeVertices.Length == 0) return;
-
-            var road = NewRoad();
-            road.positionCount = safeVertices.Length;
-            foreach (var vertex in safeVertices)
-            {
-                road.SetPosition(vertex.Position, vertex.Vector);
-            }
+//            var safeVertices = vertices.Where(vertex => vertex != null).ToArray();
+//            if (safeVertices.Length == 0) return;
+//
+//            var road = NewRoad();
+//            road.positionCount = safeVertices.Length;
+//            foreach (var vertex in safeVertices)
+//            {
+//                road.SetPosition(vertex.Position, vertex.Vector);
+//            }
         }
     }
 }
