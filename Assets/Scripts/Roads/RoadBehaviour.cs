b1bdcd63c3f194d8477175bc7528dc93929c0b93
NatalieFrecka <nfrecka@gmail.com>
2019-07-31 14:39:13 -0500

    removed shared layer and separated yellow brick road into ar road and map road

--- Assets/Scripts/Roads/RoadBehaviour.cs
+++ Assets/Scripts/Roads/RoadBehaviour.cs
@@ -1,17 +1,18 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Assets.Scripts.Roads
 {
     public class RoadBehaviour : MonoBehaviour
     {
-        public GameObject YellowBrickRoad;
+        public GameObject arYellowBrickRoad;
+        public GameObject mapYellowBrickRoad;
 
         public void Start()
         {
             foreach (var road in Repositories.RoadRepository.Get())
             {
-                YellowBrickRoad.GetComponent<YellowBrickRoad>().PathToDraw(road.Points);
+                arYellowBrickRoad.GetComponent<YellowBrickRoad>().PathToDraw(road.Points);
+                mapYellowBrickRoad.GetComponent<YellowBrickRoad>().PathToDraw(road.Points);
             }
         }
     }
