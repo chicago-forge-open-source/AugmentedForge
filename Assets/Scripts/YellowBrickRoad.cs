4d18c16619193a6c0bce69409599f368217594f1
Natalie Frecka <natalie.frecka@accenture.com>
2019-07-31 10:38:03 -0500

    adding yellow brick road script back in
    Signed-off-by: Robert Murdock <robert.f.murdock@accenture.com>

---
+++ Assets/Scripts/YellowBrickRoad.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts.Roads;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class YellowBrickRoad : MonoBehaviour
+{
+    public Material material;
+
+    private LineRenderer NewRoad()
+    {
+        var road = gameObject.AddComponent<LineRenderer>();
+        road.startWidth = 0.5f;
+        road.endWidth = 0.5f;
+        road.startColor = Color.yellow;
+        road.endColor = Color.yellow;
+        road.material = material;
+
+        return road;
+    }
+
+    public void PathToDraw(IEnumerable<RoadPoint> vertices)
+    {
+        var safeVertices = vertices.Where(vertex => vertex != null).ToArray();
+        if (safeVertices.Length == 0) return;
+
+        var road = NewRoad();
+        road.positionCount = safeVertices.Length;
+        foreach (var vertex in safeVertices)
+        {
+            road.SetPosition(vertex.Position, vertex.Vector);
+        }
+    }
+}
