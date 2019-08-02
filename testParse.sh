e6776477d61232d6f19231008c050c638dfd3b31
Riley-Pillar Mac <rmoss@pillartechnology.com>
2019-08-02 13:16:48 -0500

    Removed testParse and libxml

--- testParse.sh
+++
@@ -1,8 +0,0 @@
-result=$(echo 'cat //test-run/@result' | xmllint --shell ./test-results/editMode/results.xml | awk -F'[="]' '!/>/{print $(NF-1)}')
-
-if test "$result" = "Passed"
-then
-	exit 0;
-else
-	exit 1;
-fi
