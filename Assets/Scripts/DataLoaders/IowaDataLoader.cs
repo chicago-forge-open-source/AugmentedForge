c37be906088ecafe8032dc46b487c7381eccc01f
Riley-Pillar Mac <rmoss@pillartechnology.com>
2019-08-07 11:38:30 -0500

    Removed rotation calculation for Iowa markers, now static

--- Assets/Scripts/DataLoaders/IowaDataLoader.cs
+++ Assets/Scripts/DataLoaders/IowaDataLoader.cs
@@ -11,31 +11,23 @@ namespace DataLoaders
     {
         private const int IowaMapRotation = 28;
 
-        private readonly Marker _crucible = RotateMarkerBasedOnMapRotation(new Marker("Crucible", 0, 21.4f), IowaMapRotation);
-        private readonly Marker _focusNorth = RotateMarkerBasedOnMapRotation(new Marker("Focus North", 5.5f, 12.9f), IowaMapRotation);
-        private readonly Marker _focusMiddle = RotateMarkerBasedOnMapRotation(new Marker("Focus Middle", 5.5f, 10.4f), IowaMapRotation);
+        private readonly Marker _crucible = new Marker("Crucible", -10.01f, 18.9f);
+        private readonly Marker _focusNorth = new Marker("Focus North", -1.2f, 14.0f);
+        private readonly Marker _focusMiddle = new Marker("Focus Middle", 0f, 11.8f);
+        private readonly Marker _conferenceNorth = new Marker("Conference North", 2.0f, 8.0f);
+        private readonly Marker _kitchen = new Marker("Kitchen", -3.0f, -5.2f);
+        private readonly Marker _focusSouth = new Marker("Focus South", 11.0f, -9.0f);
+        private readonly Marker _personalHealthRoom = new Marker("Personal Health Room", 12.2f, -11.3f);
+        private readonly Marker _conferenceSouth = new Marker("Conference South", -11.4f, -20.0f);
 
-        private readonly Marker _conferenceNorth =
-            RotateMarkerBasedOnMapRotation(new Marker("Conference North", 5.5f, 6.1f), IowaMapRotation);
+        private readonly Marker _makersSpace = new Marker("Makers Space", 7.1f, -22.3f);
 
-        private readonly Marker _kitchen = RotateMarkerBasedOnMapRotation(new Marker("Kitchen", -5.1f, -3.2f), IowaMapRotation);
-        private readonly Marker _focusSouth = RotateMarkerBasedOnMapRotation(new Marker("Focus South", 5.5f, -13.1f), IowaMapRotation);
-
-        private readonly Marker _personalHealthRoom =
-            RotateMarkerBasedOnMapRotation(new Marker("Personal Health Room", 5.5f, -15.7f), IowaMapRotation);
-
-        private readonly Marker _conferenceSouth =
-            RotateMarkerBasedOnMapRotation(new Marker("Conference South", 0.7f, -23.0f), IowaMapRotation);
-
-        private readonly Marker _makersSpace =
-            RotateMarkerBasedOnMapRotation(new Marker("Makers Space", -4.2f, -23.0f), IowaMapRotation);
-
-        private readonly Marker _pathTopLeft = RotateMarkerBasedOnMapRotation(new Marker("", -2.3f, 16.5f), IowaMapRotation);
-        private readonly Marker _pathTopRight = RotateMarkerBasedOnMapRotation(new Marker("", 2.2f, 16.5f), IowaMapRotation);
-        private readonly Marker _pathBottomLeft = RotateMarkerBasedOnMapRotation(new Marker("", -2.3f, -14.3f), IowaMapRotation);
-        private readonly Marker _pathBottomRight = RotateMarkerBasedOnMapRotation(new Marker("", 2.2f, -14.3f), IowaMapRotation);
-        private readonly Marker _pathCenterLeft = RotateMarkerBasedOnMapRotation(new Marker("", -2.3f, -3.2f), IowaMapRotation);
-        private readonly Marker _pathCenterRight = RotateMarkerBasedOnMapRotation(new Marker("", 2.2f, -3.2f), IowaMapRotation);
+        private readonly Marker _pathTopLeft = new Marker("", -9.8f, 13.5f);
+        private readonly Marker _pathTopRight = new Marker("", -5.8f, 15.6f);
+        private readonly Marker _pathCenterLeft = new Marker("", -0.5f, -3.9f);
+        private readonly Marker _pathCenterRight = new Marker("", 3.4f, -1.8f);
+        private readonly Marker _pathBottomLeft = new Marker("", 4.7f, -13.7f);
+        private readonly Marker _pathBottomRight = new Marker("", 8.7f, -11.6f);
 
         public override void DataLoad()
         {
@@ -96,50 +88,6 @@ namespace DataLoaders
             {
                 new SyncPoint(0, -1.5f)
             };
-        }
-
-        //Made for rotating markers based on map rotation; Use if you make markers 
-        private static Marker RotateMarkerBasedOnMapRotation(Marker marker, float mapRotation)
-        {
-            var x = marker.X;
-            var z = marker.Z;
-
-            var vectorLength = VectorLength(x, z);
-            var sign = (z / Math.Abs(z));
-            var acos = ArcCosInDegrees(x, vectorLength);
-            var initialVectorAngle = sign * acos;
-
-            var newVectorAngle = initialVectorAngle + mapRotation;
-
-            var x1 = (float) (vectorLength * CosFromDegrees(newVectorAngle));
-            var z1 = (float) (vectorLength * SinFromDegrees(newVectorAngle));
-
-            return new Marker(marker.Label, x1, z1);
-        }
-
-        private static double VectorLength(float a, float b)
-        {
-            return Math.Sqrt(Square(a) + Square(b));
-        }
-
-        private static double Square(float num)
-        {
-            return num * num;
-        }
-
-        private static double ArcCosInDegrees(float a, double vectorLength)
-        {
-            return Math.Acos(a / vectorLength) * (180 / Math.PI);
-        }
-
-        private static double CosFromDegrees(double angle)
-        {
-            return Math.Cos(angle * (Math.PI / 180));
-        }
-
-        private static double SinFromDegrees(double angle)
-        {
-            return Math.Sin(angle * (Math.PI / 180));
         }
     }
 }
