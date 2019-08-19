19e42f91e340c897750e7dae38470a4519179d06
Natalie Frecka <natalie.frecka@accenture.com>
2019-08-19 15:09:50 -0500

    circle config to deploy to the play store
    Signed-off-by: Robert Murdock <robert.f.murdock@accenture.com>

---
+++ Assets/Editor/Export.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public static class Export
+    {
+        [MenuItem("Export/Android")]
+        public static void ExportAndroidAab()
+        {
+            var scenes = new[] {"Assets/Scenes/InitScene.unity", "Assets/Scenes/ARView.unity"};
+            const string path = "./AugmentedForge.aab";
+
+            Debug.Log("bundle version " + PlayerSettings.bundleVersion);
+            Debug.Log("bundle version code" + PlayerSettings.Android.bundleVersionCode);
+
+            var pass = Environment.GetEnvironmentVariable("KEYSTORE_PASS");
+            PlayerSettings.keystorePass = pass;
+            PlayerSettings.keyaliasPass = pass;
+            
+            BuildPipeline.BuildPlayer(scenes, path, BuildTarget.Android, BuildOptions.None);
+        }
+    }
+}
