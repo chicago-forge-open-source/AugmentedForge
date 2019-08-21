75614a49a7ae92b87cee6ed3f6ec0a5484c249b1
Jeb Sumeracki <jeb.sumeracki@accenture.com>
2019-08-21 15:54:15 -0500

    Will collect version number from the tag when doing an android build
    Signed-off-by: Robert Fuel-Synergy Murdock <robert.f.murdock@accenture.com>

--- Assets/Editor/Export.cs
+++ Assets/Editor/Export.cs
@@ -1,11 +1,35 @@
 using System;
+using System.Diagnostics;
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 namespace Editor
 {
     public static class Export
     {
+        [MenuItem("Export/TestGit")]
+        public static void LogVersion()
+        {
+            var thing = CollectTagVersion();
+
+            Debug.Log("version " + thing);
+        }
+
+        private static int CollectTagVersion()
+        {
+            var processStartInfo = new ProcessStartInfo("/usr/bin/git", "describe")
+            {
+                UseShellExecute = false,
+                RedirectStandardOutput = true
+            };
+            Process process = Process.Start(processStartInfo);
+            process?.WaitForExit();
+
+            return Int32.Parse(process.StandardOutput.ReadToEnd());
+        }
+
         [MenuItem("Export/Android")]
         public static void ExportAndroidAab()
         {
@@ -13,6 +37,10 @@ namespace Editor
             const string path = "./android-output/AugmentedForge.aab";
 
             EditorUserBuildSettings.buildAppBundle = true;
+
+            var tagVersion = CollectTagVersion();
+            PlayerSettings.Android.bundleVersionCode = tagVersion;
+            PlayerSettings.bundleVersion = $"0.0.{tagVersion}";
 
             var pass = Environment.GetEnvironmentVariable("KEYSTORE_PASS");
             if (pass != null)
