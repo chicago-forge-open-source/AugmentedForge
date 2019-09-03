d555d840bea079af5c8eb25764e79e01c904426a
Robert Murdock <robert.f.murdock@accenture.com>
2019-09-03 09:28:43 -0500

    Renaming jenkinsfile to standard name

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker image build -t jenkins-docker .
+docker image build -t jenkins-docker $(dirname $0)
 docker container run -d --name jenkins -p 8080:8080 -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock jenkins-docker
