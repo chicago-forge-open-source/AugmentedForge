d772f6f0ba6a52a8b25629c63beb20852bd96ead
Natalie Frecka <natalie.frecka@accenture.com>
2019-09-09 17:02:19 -0500

    tapping wall square changes it to red
    Signed-off-by: Jeb Sumeracki <jeb.sumeracki@accenture.com>

--- Assets/Scripts/Graffiti/MagicWallBehaviour.cs
+++ Assets/Scripts/Graffiti/MagicWallBehaviour.cs
@@ -1,21 +1,37 @@
 using System.Threading.Tasks;
+using Markers;
 using UnityEngine;
 
 namespace Graffiti
 {
     public class MagicWallBehaviour : MonoBehaviour
     {
-        private GraffitiCanvas _graffitiCanvas;
         public MeshRenderer meshRenderer;
+        public InputHandler inputHandler = new UnityInputHandler();
+        public PhysicsHandler physicsHandler = new UnityPhysicsHandler();
+        public GameObject arCameraGameObject;
+        private Camera _arCameraComponent;
+        private GraffitiCanvas _graffitiCanvas;
 
         public void Start()
         {
             _graffitiCanvas = new GraffitiCanvas();
+            _arCameraComponent = arCameraGameObject.GetComponent<Camera>();
         }
 
         public void Update()
         {
             meshRenderer.material.color = GetColorOfWall();
+
+            if (inputHandler.TouchCount <= 0) return;
+            var touch = inputHandler.GetTouch(0);
+            var touchPosition = _arCameraComponent.ScreenPointToRay(touch.position);
+            if (Equals(this, physicsHandler.Raycast<MagicWallBehaviour>(touchPosition)))
+            {
+                Task.Run(async () => { await _graffitiCanvas.UpdateMagicWallColor(Color.red); })
+                    .GetAwaiter()
+                    .GetResult();
+            }
         }
 
         private Color GetColorOfWall()
--- Assets/Tests/PlayMode/Graffiti/MagicWallBehaviourTests.cs
+++ Assets/Tests/PlayMode/Graffiti/MagicWallBehaviourTests.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Graffiti;
 using Locations;
@@ -12,23 +13,79 @@ namespace Tests.PlayMode.Graffiti
 {
     public class MagicWallBehaviourTests
     {
-        [UnityTest]
-        public IEnumerator MagicWallGetsColorAndAppliesToSelf()
+        [SetUp]
+        public void SetUp()
         {
             Repositories.LocationsRepository.Save(new[] {new Location("", "ChicagoMap")});
             Repositories.SyncPointRepository.Save(new[] {new SyncPoint("test", 10, 10, 0)});
             SetColorOfWallOnIoT(Color.blue);
             SceneManager.LoadScene("ARView");
+        }
+
+        [UnityTest]
+        public IEnumerator MagicWallGetsColorAndAppliesToSelf()
+        {
             yield return null;
             var wall = GameObject.Find("MagicWall");
             var wallColor = wall.GetComponent<MeshRenderer>().material.color;
             Assert.AreEqual(Color.blue, wallColor);
         }
 
+        [UnityTest]
+        public IEnumerator TappingGraffitiCanvasChangesColorToRed()
+        {
+            yield return null;
+            var wall = GameObject.Find("MagicWall");
+            var wallBehaviour = wall.GetComponent<MagicWallBehaviour>();
+
+            var position = wall.transform.position;
+            var touch = new Touch
+            {
+                position = position, deltaPosition = position, phase = TouchPhase.Began
+            };
+            wallBehaviour.inputHandler = new MockInputHandler(new List<Touch> {touch});
+            wallBehaviour.physicsHandler = new MockPhysicsHandler<MagicWallBehaviour>
+            {
+                ValueToReturn = wallBehaviour
+            };
+            wallBehaviour.Update();
+            yield return null;
+            yield return null;
+            var wallColor = wall.GetComponent<MeshRenderer>().material.color;
+            Assert.AreEqual(Color.red, wallColor);
+        }
+
         private static void SetColorOfWallOnIoT(Color color)
         {
             var graffitiCanvas = new GraffitiCanvas();
             Task.Run(async () => { await graffitiCanvas.UpdateMagicWallColor(color); }).GetAwaiter().GetResult();
+        }
+    }
+    
+    internal class MockInputHandler : InputHandler
+    {
+        private readonly List<Touch> _touches;
+
+        public MockInputHandler(List<Touch> touches)
+        {
+            _touches = touches;
+        }
+
+        public int TouchCount => _touches.Count;
+
+        public Touch GetTouch(int index)
+        {
+            return _touches[index];
+        }
+    }
+
+    public class MockPhysicsHandler<TR> : PhysicsHandler where TR : class
+    {
+        public TR ValueToReturn { private get; set; }
+
+        public T Raycast<T>(Ray ray) where T : class
+        {
+            return ValueToReturn as T;
         }
     }
 }
