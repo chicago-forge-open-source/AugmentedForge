657ef25c1f9dcf519f4cac82aee5bcd239ab5225
Jeb Sumaracki <jeb.sumeracki@accenture.com>
2019-09-23 15:14:15 -0500

    Removing some stuff, and finished an apparently unfinished graffiti canvas test
    Signed-off-by: Robert Murdock <robert.f.murdock@accenture.com>

--- Assets/Tests/PlayMode/Graffiti/GraffitiCanvasTests.cs
+++ Assets/Tests/PlayMode/Graffiti/GraffitiCanvasTests.cs
@@ -1,12 +1,16 @@
+using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Graffiti;
 using NUnit.Framework;
 using UnityEngine;
+using Random = System.Random;
 
 namespace Tests.PlayMode.Graffiti
 {
     public class GraffitiCanvasTests
     {
+        private readonly Random _random = new Random();
         private GraffitiCanvas _graffitiCanvas;
 
         [SetUp]
@@ -24,7 +28,26 @@ namespace Tests.PlayMode.Graffiti
         [Test]
         public void CanUpdateIoTThing()
         {
-            Task.Run(async () => { await _graffitiCanvas.UpdateGraffitiCanvasText("Hello World"); }).GetAwaiter().GetResult();
+            var expectedText = $"Hello World {_random.Next()}";
+            Task.Run(async () => { await _graffitiCanvas.UpdateGraffitiCanvasText(expectedText); }).GetAwaiter()
+                .GetResult();
+
+            Task.Run(async () =>
+            {
+                var graffitiCanvasState = await _graffitiCanvas.GetIoTThing();
+
+                var startTime = DateTime.Now;
+                while (
+                    graffitiCanvasState.text != expectedText
+                    && DateTime.Now - startTime < TimeSpan.FromSeconds(2)
+                )
+                {
+                    graffitiCanvasState = await _graffitiCanvas.GetIoTThing();
+                    Thread.Yield();
+                }
+
+                Assert.AreEqual(expectedText, graffitiCanvasState.text);
+            }).GetAwaiter().GetResult();
         }
     }
 }
